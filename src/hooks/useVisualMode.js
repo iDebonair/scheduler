9d9b4ccf89d3d7e2ff3a8418154765be6e676d64
iDebonair <ofemiowolabi@gmail.com>
2023-08-26 17:35:29 +0000

    Implemented the states to accept new and edit current schedules

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+
+export default function useVisualMode(initial){
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false){
+   setMode(newMode);
+    setHistory(prev => [...prev, newMode])
+  }
+
+  function back(){
+    if(history.length >= 1){
+      setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    }
+  }
+  return{ mode, transition, back };
+}
